aea2dc2170aadd8e4ddca8cac66c6945d105e5f9
William Cruz <cruzentwickler@gmail.com>
2021-01-28 16:16:30 +0100

    Working in the File structure and some root configurations

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Button from '@/components/Button';
+
 export default function Home() {
   return (
     <>
       <h1>William Cruz</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga soluta
+        dolores qui pariatur nemo, quam saepe quia in nam quibusdam consequuntur
+        libero aliquid, officia mollitia ullam deleniti aut dolore sapiente!
+      </p>
+      <Button>Weiter lesen</Button>
     </>
-  )
+  );
 }
