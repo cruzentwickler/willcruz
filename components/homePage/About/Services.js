a27cff312d63cec5f655deff64d8a2f1861878fd
William Cruz <cruzentwickler@gmail.com>
2021-02-09 19:18:25 +0100

    Working in the responsivnes of the About component. I isolate some partes of About component in separates compononts

--- components/homePage/About/Services.js
+++ components/homePage/About/Services.js
@@ -1,13 +1,103 @@
 import React from 'react';
 import Link from 'next/link';
 
-const Services = ({ data }) => {
+const Services = () => {
+  const data = [
+    {
+      id: '1',
+      title: 'Design',
+      description: [
+        {
+          name: 'Web design',
+          url: '/'
+        },
+        {
+          name: 'Art direction',
+          url: '/'
+        },
+        {
+          name: 'UI & UX',
+          url: '/'
+        }
+      ]
+    },
+    {
+      id: '2',
+      title: 'Digital & Mobile',
+      description: [
+        {
+          name: 'Websites',
+          url: '/'
+        },
+        {
+          name: 'Responsive design',
+          url: '/'
+        },
+        {
+          name: 'Mobile & Tablet',
+          url: '/'
+        },
+        {
+          name: 'Mobile apps (iOS, Android)',
+          url: '/'
+        }
+      ]
+    },
+    {
+      id: '3',
+      title: 'Web Development',
+      description: [
+        {
+          name: 'Front end & Back end',
+          url: '/'
+        },
+        {
+          name: 'Interaction design',
+          url: '/'
+        },
+        {
+          name: 'Magento, Wordpress',
+          url: '/'
+        },
+        {
+          name: 'Symfony, Laravel',
+          url: '/'
+        }
+      ]
+    },
+    {
+      id: '4',
+      title: 'Social Media & Webmarketing',
+      description: [
+        {
+          name: 'Social Media Strategy',
+          url: '/'
+        },
+        {
+          name: 'Community Management',
+          url: '/'
+        },
+        {
+          name: 'Bloggers & influencers',
+          url: '/'
+        },
+        {
+          name: 'Reporting, live events',
+          url: '/'
+        },
+        {
+          name: 'Mailing & competition',
+          url: '/'
+        }
+      ]
+    }
+  ];
+
   return (
     <div className="border grid sm:grid-cols-2">
       {data.map((item) => (
-        <ul key={item.id} className="p-4">
-          <h3 className="font-bold text-lg mb-1">{item.title}</h3>
-
+        <ul key={item.id} className="p-4 text-lg">
+          <h3 className="font-bold mb-1">{item.title}</h3>
           {item.description.map((item) => (
             <li key={item.name}>
               <Link href={item.url}>
--- components/homePage/About/index.js
+++ components/homePage/About/index.js
@@ -1,123 +1,14 @@
-import Link from 'next/link';
-import Title from '@/components/Title';
+import Content from '@/components/homePage/About/Content';
 import Services from '@/components/homePage/About/Services';
+
 const About = () => {
-  const data = [
-    {
-      id: '1',
-      title: 'Design',
-      description: [
-        {
-          name: 'Web design',
-          url: '/'
-        },
-        {
-          name: 'Art direction',
-          url: '/'
-        },
-        {
-          name: 'UI & UX',
-          url: '/'
-        }
-      ]
-    },
-    {
-      id: '2',
-      title: 'Digital & Mobile',
-      description: [
-        {
-          name: 'Websites',
-          url: '/'
-        },
-        {
-          name: 'Responsive design',
-          url: '/'
-        },
-        {
-          name: 'Mobile & Tablet',
-          url: '/'
-        },
-        {
-          name: 'Mobile apps (iOS, Android)',
-          url: '/'
-        }
-      ]
-    },
-    {
-      id: '3',
-      title: 'Web Development',
-      description: [
-        {
-          name: 'Front end & Back end',
-          url: '/'
-        },
-        {
-          name: 'Interaction design',
-          url: '/'
-        },
-        {
-          name: 'Magento, Wordpress',
-          url: '/'
-        },
-        {
-          name: 'Symfony, Laravel',
-          url: '/'
-        }
-      ]
-    },
-    {
-      id: '4',
-      title: 'Social Media & Webmarketing',
-      description: [
-        {
-          name: 'Social Media Strategy',
-          url: '/'
-        },
-        {
-          name: 'Community Management',
-          url: '/'
-        },
-        {
-          name: 'Bloggers & influencers',
-          url: '/'
-        },
-        {
-          name: 'Reporting, live events',
-          url: '/'
-        },
-        {
-          name: 'Mailing & competition',
-          url: '/'
-        }
-      ]
-    }
-  ];
-
   return (
-    <section className="grid min-h-screen md:grid-cols-2 ">
-      <div className="content-center grid justify-center p-8">
-        <div className="border md:max-w-xs grid gap-8 p-4">
-          <Title title="Digital" />
-          <p className="text-4xl font-bold max-w-xs">
-            An <span className="font-handwriting">overview</span> of your
-            digital strategy
-            <span className="">.</span>
-          </p>
-          <p className="text-lg">
-            A key component of your transformation, your digital strategy
-            requires special attention. We provide you with the support you need
-            all during your project. Through regular workshops, brainstormings
-            and idea sharing, we help grow your initial idea into a cutting-edge
-            digital solution.
-          </p>
-
-          <Link href="/">
-            <a className="font-handwriting text-2xl">Weiter lesen</a>
-          </Link>
-        </div>
+    <section className="grid min-h-screen lg:grid-cols-2 ">
+      <div className="content-center grid justify-center">
+        <Content />
       </div>
       <div className="content-end grid">
-        <Services data={data} />
+        <Services />
       </div>
     </section>
   );
